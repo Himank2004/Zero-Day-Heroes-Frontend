2147c1aaa87d4d83f2cff3f5ec71b2aea93bb334
Anurag Singh <iitpatelanurag2003@gmail.com>
2025-04-10 18:30:00 +0530

    workflow

--- src/Components/singup.jsx
+++ src/Components/singup.jsx
@@ -86,19 +86,7 @@ const SignUpForm = () => {
               Sign up with Google
             </button>
           </div>
-          <div>
-            <button
-              type="button"
-              className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out"
-            >
-              <img
-                className="ml-3 w-5 h-5 mr-2"
-                src={onlyFnas}
-                alt="OnlyFans logo"
-              />
-              Sign up with OnlyFans
-            </button>
-          </div>    
+             
           {/* Divider */}
           <div className="relative mt-6">
             <div className="absolute inset-0 flex items-center">
